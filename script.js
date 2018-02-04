3274188c3448d510228d66c38929e23a52e50a21
SujiHan <sujihan2135@gmail.com>
2018-02-03 23:03:38 -0500

    add all files

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  $(".trigger_popup_fricc1").click(function(){
+     $('.hover_bkgr_fricc1').show();
+  });
+  $('.hover_bkgr_fricc1').click(function(){
+      $('.hover_bkgr_fricc1').hide();
+  });
+  $('.popupCloseButton1').click(function(){
+      $('.hover_bkgr_fricc1').hide();
+  });
+
+  $(".trigger_popup_fricc2").click(function(){
+     $('.hover_bkgr_fricc2').show();
+  });
+  $('.hover_bkgr_fricc2').click(function(){
+      $('.hover_bkgr_fricc2').hide();
+  });
+  $('.popupCloseButton2').click(function(){
+      $('.hover_bkgr_fricc2').hide();
+  });
+
+  $(".trigger_popup_fricc3").click(function(){
+     $('.hover_bkgr_fricc3').show();
+  });
+  $('.hover_bkgr_fricc3').click(function(){
+      $('.hover_bkgr_fricc3').hide();
+  });
+  $('.popupCloseButton3').click(function(){
+      $('.hover_bkgr_fricc3').hide();
+  });
+
+  $(".trigger_popup_fricc4").click(function(){
+     $('.hover_bkgr_fricc4').show();
+  });
+  $('.hover_bkgr_fricc4').click(function(){
+      $('.hover_bkgr_fricc4').hide();
+  });
+  $('.popupCloseButton4').click(function(){
+      $('.hover_bkgr_fricc4').hide();
+  });
+
+
+  });
+
+
+// });
